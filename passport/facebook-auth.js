90d3efedbdf812ce028e171b46be45f76efc3960
Esteban Ibañez <e.ibannez.p@gmail.com>
2021-08-10 22:23:54 -0400

    wip

--- passport/facebook-auth.js
+++ passport/facebook-auth.js
@@ -1,25 +1,25 @@
 const FacebookStrategy = require("passport-facebook").Strategy;
+const config = require('../config/config')
 
 // completar con sus credenciales
-// const FACEBOOK_CLIENT_ID = process.env.FACEBOOK_CLIENT_ID;
-// const FACEBOOK_CLIENT_SECRET = process.env.FACEBOOK_CLIENT_SECRET;
-
+const FACEBOOK_CLIENT_ID = config.FACEBOOK_CLIENT_ID;
+const FACEBOOK_CLIENT_SECRET = config.FACEBOOK_CLIENT_SECRET;
 
 // ------- configuramos passport para usar facebook
-// passport.use(
-//   new FacebookStrategy(
-//     {
-//       clientID: FACEBOOK_CLIENT_ID,
-//       clientSecret: FACEBOOK_CLIENT_SECRET,
-//       callbackURL: "/api/auth/facebook/callback",
-//       profileFields: ["id", "displayName", "photos", "emails"],
-//       scope: ["email"],
-//     },
-//     function (accessToken, refreshToken, profile, done) {
-//       let userProfile = profile._json;
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: FACEBOOK_CLIENT_ID,
+      clientSecret: FACEBOOK_CLIENT_SECRET,
+      callbackURL: "/api/auth/facebook/callback",
+      profileFields: ["id", "displayName", "photos", "emails"],
+      scope: ["email"],
+    },
+    function (accessToken, refreshToken, profile, done) {
+      let userProfile = profile._json;
 
-//       console.log("Perfil de usuario logeado : ", userProfile);
-//       return done(null, userProfile);
-//     },
-//   ),
-// );
+      console.log("Perfil de usuario logeado : ", userProfile);
+      return done(null, userProfile);
+    },
+  ),
+);
